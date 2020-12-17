60a0bf1cee570db3ee1d475f9a36cc6deef1d956
xrkffgg <xrkffgg@vip.qq.com>
2020-12-17 20:32:52 +0800

    add

--- tests/hover.spec.tsx
+++ tests/hover.spec.tsx
@@ -8,19 +8,10 @@ describe('Hover', () => {
   const testSrc =
     'https://github.com/image-component/gallery/blob/main/girl/1.jpg?raw=true';
 
-  it('mouse', () => {
-    const wrapper = mount(
-      <ImageDangling src={testSrc} hoverScale={1.4} hoverSlope={40} />,
-    );
-    const node = wrapper.find('.react-image-dangling');
-    node.simulate('mouseOver', {});
-    expect(
-      wrapper.find('.react-image-dangling-card').prop('style').opacity,
-    ).toEqual(1);
+  let originOffsetWidth;
+  let originOffsetHeight;
 
-    let originOffsetWidth;
-    let originOffsetHeight;
-
+  beforeAll(() => {
     originOffsetWidth = Object.getOwnPropertyDescriptor(
       HTMLElement.prototype,
       'offsetWidth',
@@ -40,22 +31,9 @@ describe('Hover', () => {
         return 100;
       },
     });
+  });
 
-    node.simulate('mouseMove', {
-      offsetX: 99,
-      offsetY: 99,
-    });
-
-    console.log(wrapper.find('.react-image-dangling').prop('style'));
-
-    node.simulate('mouseLeave', {});
-    expect(
-      wrapper.find('.react-image-dangling').prop('style').transform,
-    ).toEqual('perspective(500px) scale(1)');
-    expect(
-      wrapper.find('.react-image-dangling-card').prop('style').opacity,
-    ).toEqual(0);
-
+  afterAll(() => {
     Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
       get: originOffsetWidth,
     });
@@ -64,14 +42,47 @@ describe('Hover', () => {
     });
   });
 
+  it('mouse', () => {
+    const wrapper = mount(
+      <ImageDangling src={testSrc} hoverScale={1.4} hoverSlope={40} />,
+    );
+    const node = wrapper.find('.react-image-dangling');
+    node.simulate('mouseOver', {});
+    expect(
+      wrapper.find('.react-image-dangling-card').prop('style').opacity,
+    ).toEqual(1);
+
+    node.simulate('mouseMove', {
+      nativeEvent: {
+        offsetX: 99,
+        offsetY: 99,
+      },
+    });
+
+    expect(
+      wrapper.find('.react-image-dangling').prop('style').transform,
+    ).toEqual('perspective(500px) scale(1.4) rotateY(20deg) rotateX(-20deg)');
+
+    node.simulate('mouseLeave', {});
+
+    expect(
+      wrapper.find('.react-image-dangling').prop('style').transform,
+    ).toEqual('perspective(500px) scale(1)');
+    expect(
+      wrapper.find('.react-image-dangling-card').prop('style').opacity,
+    ).toEqual(0);
+  });
+
   it('blendMode', () => {
     const wrapper = mount(<ImageDangling src={testSrc} blendMode />);
 
     const node = wrapper.find('.react-image-dangling');
 
     node.simulate('mouseMove', {
-      offsetX: 99,
-      offsetY: 99,
+      nativeEvent: {
+        offsetX: 99,
+        offsetY: 99,
+      },
     });
 
     expect(
@@ -79,22 +90,38 @@ describe('Hover', () => {
     ).toEqual('soft-light');
 
     expect(
+      wrapper.find('.react-image-dangling-card').prop('style').backgroundImage,
+    ).toEqual(
+      'radial-gradient(circle at -50% -100%, rgba(109.28571428571429, 109.28571428571429, 109.28571428571429, 1) 20%, #000)',
+    );
+
+    expect(
       wrapper.find('.react-image-dangling-card').prop('style').opacity,
     ).toEqual(1);
   });
 
   it('blendMode customer', () => {
-    const wrapper = mount(<ImageDangling src={testSrc} blendMode="color" />);
+    const wrapper = mount(
+      <ImageDangling src={testSrc} blendMode="color" shadeColor="#001" />,
+    );
 
     const node = wrapper.find('.react-image-dangling');
 
     node.simulate('mouseMove', {
-      offsetX: 99,
-      offsetY: 99,
+      nativeEvent: {
+        offsetX: 99,
+        offsetY: 99,
+      },
     });
 
     expect(
       wrapper.find('.react-image-dangling-card').prop('style').mixBlendMode,
     ).toEqual('color');
+
+    expect(
+      wrapper.find('.react-image-dangling-card').prop('style').backgroundImage,
+    ).toEqual(
+      'radial-gradient(circle at -50% -100%, rgba(109.28571428571429, 109.28571428571429, 109.28571428571429, 1) 20%, #001)',
+    );
   });
 });
